a9000514e32f18c8161c95f82b6608a433f5e2e5
Sunil <sunilsahare14@gmail.com>
2024-04-18 22:54:37 +0530

    Added Service REgistry and sample User service

---
+++ Backend/ecommerce-user-service/src/main/java/com/ecommerce/userservice/EcommerceUserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ecommerce.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EcommerceUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceUserServiceApplication.class, args);
+	}
+
+}
